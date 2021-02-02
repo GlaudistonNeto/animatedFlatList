dccf8b641101cc8d13f5d330100018187277d97e
gneto <glaudistonson@gmail.com>
2021-02-02 02:38:03 -0300

    exemple of animated flatList using expo

---
+++ components/Onboarding.js
@@ -0,0 +1,48 @@
+import React, { useRef, useState } from 'react';
+import { StyleSheet, View, FlatList, Animated } from 'react-native';
+
+import OnboardingItem from './OnboardingItem';
+import  slides from '../slids';
+
+export default Onboarding = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const slideRef = useRef(null);
+
+  const viewableItemsChanged = useRef(({ viewableItems }) => {
+    setCurrentIndex(viewableItems[0].index);
+  }).current;
+
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+
+ return ( 
+ <View style={styles.container}>
+   <View style={{ flex: 3 }}>
+     <FlatList
+       data={slides}
+       renderItem={({ item }) => <OnboardingItem item={item}/>}
+       horizontal
+       showsHorizontalScrollIndicator
+       pagingEnabled
+       bounces={false}
+       keyExtractor={(item) => item.id}
+       onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], {
+         useNativeDriver: false,
+       })}
+       scrollEventThrottle={32}
+       onViewableItemsChanged={viewableItemsChanged}
+       viewabilityConfig={viewConfig}
+       ref={slideRef}
+     />
+   </View>
+  </View> 
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+  },
+});
